018fb36891de94260d4457bebe6c5fb3c47057bb
Hamza Khalid <hamzer95@gmail.com>
2021-11-04 18:00:41 -0400

    added TypeDefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,38 @@
+const {gql} = require('apollo-server-express');
+
+const typeDefs = gql`
+type User {
+    _id: ID!
+    username: String!
+    email: String!
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+      bookId: String!
+      authors: [String]
+      title: String!
+      description: String!
+      image: String
+      link : String
+  }
+
+  type Auth {
+      token: ID!
+      user: User
+  }
+
+  type Query {
+      me: User
+  }
+
+  type mutation{
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookId: String!, title: String!, description: String!, authors: [String], image: String, link: String): User
+    removeBook(bookId: String!): User
+}
+
+
+`
